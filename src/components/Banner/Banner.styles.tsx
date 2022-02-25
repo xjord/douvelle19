9a943f17465e913a54b9e3f07bc74834a7eabf44
Jordan Wood <jordanwood1993@yahoo.co.uk>
2022-02-25 11:44:04 +0000

    Some banner styling tweaks

--- src/components/Banner/Banner.styles.tsx
+++ src/components/Banner/Banner.styles.tsx
@@ -1,33 +1,46 @@
-import { createUseStyles } from "react-jss";
+import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
   bannerWrapper: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
   },
   banner: {
-    display: "flex",
-    justifyContent: "center",
-    color: "white",
+    display: 'flex',
+    justifyContent: 'center',
+    color: 'white',
   },
   bannerImageWrapper: {
-    marginRight: 24,
-    cursor: "pointer",
+    marginRight: 100,
+    cursor: 'pointer',
+    borderRadius: 8,
+    boxShadow: {
+      x: -4,
+      y: -4,
+      blur: 16,
+      color: '#000000',
+    },
   },
   bannerImage: {
-    borderRadius: 4,
-    height: "50vmin",
-    "&:hover": {
+    borderRadius: 8,
+    height: '50vmin',
+    '&:hover': {
       opacity: 0.8,
     },
   },
+  bannerTitle: {
+    fontSize: 42,
+  },
   bannerText: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  bannerOutNow: {
+    fontSize: 22,
   },
   bannerButtonWrapper: {
     marginTop: 8,
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
-import { openInTab } from "../../utils/helpers";
-import Button from "../Button";
-import useStyles from "./Banner.styles";
+import { openInTab } from '../../utils/helpers';
+import Button from '../Button';
+import useStyles from './Banner.styles';
 
 const Home = ({ banner }) => {
-
   const releaseTitle = banner?.releaseTitle;
   const releaseLink = banner?.link?.content[0]?.content[0]?.value;
   const releaseImage = banner?.bannerImage?.fields?.file?.url;
@@ -21,17 +20,16 @@ const Home = ({ banner }) => {
             className={classes.bannerImage}
             height={500}
             width={500}
-            src={"https:" + releaseImage}
+            src={'https:' + releaseImage}
             alt={releaseTitle}
             onClick={() => openInTab(releaseLink)}
           />
         </div>
         <div className={classes.bannerText}>
-          <div>{releaseTitle}</div>
+          <div className={classes.bannerTitle}>{releaseTitle}</div>
+          <div className={classes.bannerOutNow}>OUT NOW</div>
           <div className={classes.bannerButtonWrapper}>
-            <Button onClick={() => openInTab(releaseLink)}>
-              Stream/Purchase here
-            </Button>
+            <Button onClick={() => openInTab(releaseLink)}>Stream/Purchase</Button>
           </div>
         </div>
       </div>
