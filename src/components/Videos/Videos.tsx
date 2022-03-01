2e12edf5c83972f688a4eedee424d163573f0477
Jordan Wood <jordan.wood@sgdigital.com>
2022-03-01 22:09:53 +0000

    Added video thumbnail with playbutton

--- src/components/Videos/Videos.tsx
+++ src/components/Videos/Videos.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import YouTube from 'react-youtube';
 import Image from 'next/image';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import useStyles from './Videos.styles';
 
 const Music = (props) => {
   const { videos } = props;
 
-  const link = videos[0].fields.url.content[0].content[0].value;
-
-  const thumbnail = `https://img.youtube.com/vi/${link}/0.jpg`;
-
   const classes = useStyles();
 
   return (
     <div className={classes.videos}>
-      {/* Map through videos here */}
-      <YouTube videoId={link} />
-      <Image className={classes.videoThumbnail} height={400} width={640} src={thumbnail} alt={thumbnail} />
-      {/* Add play button on hover */}
+      <div>
+        {videos.map((video, index) => {
+          const videoId = video.fields.videoId;
+          const thumbnail = `https://img.youtube.com/vi/${videoId}/0.jpg`;
+          return (
+            <div key={index} className={classes.video}>
+              <div className={classes.videoPlayIconWrapper}>
+                {/* onClick={() => openSocial(link)} */}
+                <FontAwesomeIcon className={classes.videoPlayIcon} icon={faPlay} />
+              </div>
+              {/* <YouTube videoId={link} /> */}
+              <Image className={classes.videoThumbnail} height={400} width={640} src={thumbnail} alt={thumbnail} />
+            </div>
+          );
+        })}
+      </div>
       {/* Open modal with video in on click of play */}
     </div>
   );
