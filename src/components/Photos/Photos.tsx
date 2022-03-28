34a1f89b1bb614cf05419d69810e6e93c17fdd06
Jordan Wood <jordanwood1993@yahoo.co.uk>
2022-03-28 22:37:24 +0100

    Add instagram token for now

--- src/components/Photos/Photos.tsx
+++ src/components/Photos/Photos.tsx
@@ -9,7 +9,10 @@ import { InstagramWrapper, FollowButtonWrapper, FollowButton } from './Photos.st
 const Photos = () => {
   return (
     <InstagramWrapper>
-      <InstagramFeed token={process.env.INSTAGRAM_ACCESS_TOKEN} counter="24" />
+      <InstagramFeed
+        token="IGQVJYT1NsLTN0b3dtbl9teGdYTUtOWEVyTHQ1YWlwcmw5VXpsOWJsRzBaSHc5RllMYXh4OTE5dldOdU9vSV9kejFONFh6RlgtSzk0UXAyVTMyd1FRR0E2eF94VnBiTEZAvejk5ZAU85LVpRbERJaWYzdQZDZD"
+        counter="24"
+      />
       <FollowButtonWrapper>
         <FollowButton>
           <Button onClick={() => openInTab('https://www.instagram.com/douvelle19/')}>Follow</Button>
