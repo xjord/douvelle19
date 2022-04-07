5d78a4cdf41b3192ea4d91aaf12e05d5e516d613
Jordan Wood <jordanwood1993@yahoo.co.uk>
2022-04-07 18:55:44 +0100

    test sitemap path

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -12,7 +12,7 @@ export const getServerSideProps = async ({ res }) => {
     .readdirSync(
       {
         development: 'pages',
-        production: './',
+        production: './.next/server/pages',
       }[process.env.NODE_ENV],
     )
     .filter((staticPage) => {
