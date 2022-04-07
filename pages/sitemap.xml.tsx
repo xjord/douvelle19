d8c1e757dd989536721476c2503eac464ab7fcb2
Jordan Wood <jordanwood1993@yahoo.co.uk>
2022-04-07 18:04:20 +0100

    Added sitemap

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,41 @@
+import * as fs from 'fs';
+
+const Sitemap = () => null;
+
+export const getServerSideProps = async ({ res }) => {
+  const paths = fs
+    .readdirSync('pages')
+    .filter((staticPage) => {
+      return !['api', '_app.tsx', '_document.tsx', '404.tsx', 'sitemap.xml.tsx'].includes(staticPage);
+    })
+    .map((pagePath) => {
+      return `www.douvelle19.com/${pagePath}`;
+    });
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${paths
+        .map((url) => {
+          return `
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+        })
+        .join('')}
+    </urlset>
+`;
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
