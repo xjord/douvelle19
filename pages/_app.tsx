5a4d3dcb83a6dfcd27cf2b5eeb962fa35b06c252
Jordan Wood <jordanwood1993@yahoo.co.uk>
2022-04-08 18:22:44 +0100

    Moved font to inside Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,43 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Script from 'next/script';
 
 import Layout from '../src/components/Layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout {...pageProps}>
-      <Component {...pageProps} />;
-    </Layout>
+    <>
+      <Script
+        id="gtagmanager"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      />
+      ;
+      <Script id="gtag" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', ${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID});
+        `}
+      </Script>
+      <Head>
+        <title>Douvelle19</title>
+        <meta name="description" content="Douvelle19 music producer and dj" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="UTF-8" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+      </Head>
+      <Layout {...pageProps}>
+        <Component {...pageProps} />;
+      </Layout>
+    </>
   );
 }
 
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
-import Script from 'next/script';
 
 import Header from '../Header';
 import Socials from '../Socials';
@@ -23,29 +21,6 @@ const Layout = (props) => {
 
   return (
     <>
-      <Script
-        id="gtagmanager"
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-      />
-      ;
-      <Script id="gtag" strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          
-          gtag('config', ${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID});
-        `}
-      </Script>
-      <Head>
-        <title>Douvelle19</title>
-        <meta name="description" content="Douvelle19 music producer and dj" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta charSet="UTF-8" />
-        <meta name="robots" content="index, follow" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       {logo && <Header logo={logo} />}
       <Socials socials={socials} />
       <Navigation />
